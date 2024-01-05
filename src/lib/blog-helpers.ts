43d57cde98110a1b7738a66ab3942e561cb31eed
kkosuge <root@kksg.net>
2024-01-05 18:10:39 +0900

    Revert "feat: amazon bookmark"
    This reverts commit cae006d64e5cdb865f824f6e262491aaa4454f17.

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch'
-import { chromium } from '@playwright/test'
 import { BASE_PATH, REQUEST_TIMEOUT_MS } from '../server-constants'
 import type {
   Block,
@@ -87,15 +86,6 @@ const _extractTargetBlockFromColums = (
     .flat()
 }
 
-async function getAmazonHtml(url: string) {
-  const browser = await chromium.launch()
-  const page = await browser.newPage()
-  await page.goto(url, { waitUntil: 'load' })
-  const html = await page.content()
-  await browser.close()
-  return html
-}
-
 export const buildURLToHTMLMap = async (
   urls: URL[]
 ): Promise<{ [key: string]: string }> => {
@@ -106,31 +96,17 @@ export const buildURLToHTMLMap = async (
         controller.abort()
       }, REQUEST_TIMEOUT_MS)
 
-      if (isAmazonURL(url)) {
-        return getAmazonHtml(url.toString())
-          .then((html) => {
-            return html
-          })
-          .catch(() => {
-            console.log('Request was aborted')
-            return ''
-          })
-          .finally(() => {
-            clearTimeout(timeout)
-          })
-      } else {
-        return fetch(url.toString(), { signal: controller.signal })
-          .then((res) => {
-            return res.text()
-          })
-          .catch(() => {
-            console.log('Request was aborted')
-            return ''
-          })
-          .finally(() => {
-            clearTimeout(timeout)
-          })
-      }
+      return fetch(url.toString(), { signal: controller.signal })
+        .then((res) => {
+          return res.text()
+        })
+        .catch(() => {
+          console.log('Request was aborted')
+          return ''
+        })
+        .finally(() => {
+          clearTimeout(timeout)
+        })
     })
   )
 
@@ -270,7 +246,6 @@ export const isFullAmazonURL = (url: URL): boolean => {
   return false
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const isAmazonURL = (url: URL): boolean => {
   return isShortAmazonURL(url) || isFullAmazonURL(url)
 }
